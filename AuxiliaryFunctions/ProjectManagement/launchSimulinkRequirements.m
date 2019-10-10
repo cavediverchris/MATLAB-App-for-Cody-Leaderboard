5f4e6c7e61cef28f10725c7c736cc509a8dca26f
chris <chris@DESKTOP-36LNQQM.home>
2019-10-10 18:58:04 +0100

    Updated launch script to check for Simulink Requirements - if not a message is pushed to the display to avoid an error.

--- AuxiliaryFunctions/ProjectManagement/launchSimulinkRequirements.m
+++ AuxiliaryFunctions/ProjectManagement/launchSimulinkRequirements.m
@@ -1,3 +1,14 @@
 %% Launch Simulink Requirements Editor
 
-slreq.open('ProjectRequirements');
+% Need to check if Simulink Requirements licence even exists
+% TODO - Need the product name
+% status = license('test','slrequirements');
+
+% Alternatively catch the error message (if one)
+try
+    slreq.open('ProjectRequirements');
+catch ME
+    if strcmp(ME.identifier, 'Slvnv:slreq:FailedToOpen')
+        disp('Simulink Requirements not installed.')
+    end
+end
